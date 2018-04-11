84d41901064064dce05cfe80dcd557348551f21d
Nuno Aido <nuno.c.aido@gmail.com>
2018-04-11 21:21:07 +0100

    Adicionar ficheiro veiculos

---
+++ Oper_NA.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Nuno
+ */
+public class Oper_NA {
+    	/*Quadrado da soma de dois numeros*/
+        public double oper(double op1, double op2) {
+		return (op1+op2)*(op1+op2);
+	}
+}
